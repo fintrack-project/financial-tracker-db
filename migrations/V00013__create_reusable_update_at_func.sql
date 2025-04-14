dbf5b3cc19cc33c40a32bc5cd5dc0b0e6fe16069
johnkim1216 <johnmjkim1216@gmail.com>
2025-04-14 11:47:54 +1000

    add tables for categories

---
+++ migrations/V00013__create_reusable_update_at_func.sql
@@ -0,0 +1,8 @@
+-- Create a reusable trigger function to update the updated_at column
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
---
+++ migrations/V00015__create_holdings_categories_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE holdings_categories (
+    id SERIAL PRIMARY KEY, -- Unique identifier for the relationship
+    account_id UUID NOT NULL REFERENCES users(account_id) ON DELETE CASCADE, -- Links to the user
+    asset_name VARCHAR(255) NOT NULL, -- Name of the asset (e.g., Bitcoin, USD)
+    category_id INT NOT NULL REFERENCES categories(category_id) ON DELETE CASCADE, -- Links to the category
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Tracks the last update time
+    UNIQUE (account_id, asset_name) -- Ensure each asset_name is linked to only one category per user
+);
+
+-- Attach the reusable trigger to the holdings_categories table
+CREATE TRIGGER trigger_update_holdings_categories_updated_at
+BEFORE UPDATE ON holdings_categories
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
