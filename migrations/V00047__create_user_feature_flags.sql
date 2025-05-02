0daf65000f34d0318ffc5ae99f5e63234d0811ae
johnkim1216 <johnmjkim1216@gmail.com>
2025-05-02 16:06:34 +1000

    replace using jsonb

---
+++ migrations/V00047__create_user_feature_flags.sql
@@ -0,0 +1,7 @@
+-- Table to store feature flags for each user
+CREATE TABLE user_feature_flags (
+    id SERIAL PRIMARY KEY,
+    account_id UUID NOT NULL REFERENCES users(account_id) ON DELETE CASCADE,
+    feature_flag VARCHAR(255) NOT NULL, -- Name of the feature flag
+    is_enabled BOOLEAN NOT NULL DEFAULT FALSE -- Whether the feature is enabled
+);
